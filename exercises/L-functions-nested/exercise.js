21256349d28a022b8a88dda0487681a3598961da
Adepeju <pejulawson18@gmail.com>
2021-04-20 23:10:29 +0100

    finished the exercises

--- exercises/L-functions-nested/exercise.js
+++ exercises/L-functions-nested/exercise.js
@@ -3,3 +3,20 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+function greet(mentor){
+    return ("Hello "+ mentor);
+
+}
+function spellName(mentor, greet){
+    return(mentor + greet.toUpperCase);
+    
+
+}
+console.log(greet(mentor1).toUpperCase());
+console.log(greet(mentor2).toUpperCase());
+console.log(greet(mentor3).toUpperCase());
+console.log(greet(mentor4).toUpperCase());
+console.log(greet(mentor5).toUpperCase());
+
+
+
